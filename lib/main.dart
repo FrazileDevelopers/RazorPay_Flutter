519f50aa05452a2bcb765f49b5fc8c8383272f9b
Parth Aggarwal <frazile.com@gmail.com>
2020-02-29 21:58:07 +0530

    Bugs Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 void main() => runApp(RazorPay());
 
@@ -22,6 +24,60 @@ class Pay extends StatefulWidget {
 }
 
 class _PayState extends State<Pay> {
+  Razorpay _razorpay;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _razorpay = Razorpay();
+    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _razorpay.clear();
+    super.dispose();
+  }
+
+  void openCheckout() async {
+    var options = {
+      'key': 'rzp_live_tH6FiVUcs8nlTt',
+      'amount': 1000,
+      'name': 'Donate',
+      'description': 'Donation',
+      'prefill': {'contact': '9988446981', 'email': 'frazile.com@gmail.com'},
+      'external': {
+        'wallets': ['paytm']
+      }
+    };
+
+    try {
+      _razorpay.open(options);
+    } catch (e) {
+      debugPrint(e);
+    }
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "SUCCESS: " + response.paymentId, timeInSecForIos: 4);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "ERROR: " + response.code.toString() + " - " + response.message,
+        timeInSecForIos: 4);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: "EXTERNAL_WALLET: " + response.walletName, timeInSecForIos: 4);
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
@@ -30,7 +86,8 @@ class _PayState extends State<Pay> {
         ),
         body: Center(
           child: MaterialButton(
-            onPressed: () {},
+            color: Colors.pink,
+            onPressed: openCheckout,
             child: Text(
               'Donate INR 100.00',
               style: TextStyle(
